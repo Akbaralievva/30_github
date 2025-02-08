1d48a286d771c8408afc5bdd59414be7bfafc542
Akbaralievva <alinaakbaralieva0@gmail.com>
2025-02-08 22:39:59 +0600

    Initial Commit

---
+++ 30_githubTests/_0_githubTests.swift
@@ -0,0 +1,17 @@
+//
+//  _0_githubTests.swift
+//  30_githubTests
+//
+//  Created by A LINA on 8/2/25.
+//
+
+import Testing
+@testable import _0_github
+
+struct _0_githubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
